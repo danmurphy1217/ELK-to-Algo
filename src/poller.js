72724fa0217f7d1602d5150405c9b4db72cc3cc1
Dan Murphy <danielmurph8@gmail.com>
2021-08-07 12:57:11 -0400

    reformat class structure into three different classes, one that focuses on reading data, one that focuses on structuring it, and another that uploads to elasticsearch

--- src/poller.js
+++ src/poller.js
@@ -1,87 +1,270 @@
-const { AlgoLogger: AlgoLogger } = require('./index.js')
+const {
+  Client
+} = require('@elastic/elasticsearch')
+const fs = require('fs')
+const fetch = require('node-fetch');
 
+class Reader {
+  /**
+   * reads in data, whether it be from a file or
+   * an API endpoint
+   */
 
-class Poller {
-
-  every(nSeconds) {
-    this.interval = this._convertToMilliseconds(nSeconds);
-
-    return new Iterator(this)
+  constructor(location, keysToFetch = null, {
+    headers,
+    params,
+    body
+  } = {}) {
+    /**
+     * `location` -> (String): the location to read the
+     * data from
+     */
+    this.location = location
+    this.keysToFetch = keysToFetch
+    this.headers = headers;
+    this.params = params
+    this.body = body
   }
 
-  _convertToMilliseconds(nSeconds) {
-    return nSeconds * 1000;
+  async read() {
+
+    let response;
+    response = await this._pingEndpoint(this.location)
+
+    return new Parser(this);
   }
 
-}
+  async _pingEndpoint() {
+    /**
+     * ping `this.location` and return JSON data for top-transactions.
+     */
 
+    let json;
 
-class Iterator {
-  constructor(poller) {
-    this.poller = poller;
-  }
-
-  for(nIterations) {
-    this.iterations = nIterations;
-
-    return new Runner(this)
-  }
-}
-
-class Runner {
-  constructor(iterator) {
-    this.iterator = iterator
-  }
-
-
-  run(fn) {
-    let interval = this.iterator.poller.interval;
-    let iterations = this.iterator.iterations;
-
-    let result;
-    if (iterations === -1) {
-      result = this._runWithInfiniteAttempts(interval, fn)
-    } else {
-      result = this._runWithFiniteAttempts(interval, iterations, fn)
+    try {
+      let res = await fetch(this.location, {
+        headers: this.headers
+      })
+      json = await res.json();
+    } catch (err) {
+      return new Error(err);
     }
 
-    return new Promise(result);
+    this.data = json['top-transactions'];
   }
 
-  _runWithInfiniteAttempts(interval, fn) {
+}
 
-
-    // console.log(interval);
-    // let result = setInterval(fn, interval);
-    // console.log(result);
+class Parser {
+  constructor(reader) {
+    this.reader = reader
   }
 
-  _runWithFiniteAttempts(interval, iterations, fn) {
-    let attempts = 0;
+  async structure() {
+    /**
+     * structure data in the log files for
+     * elasticsearch
+     * unstructuredData -> (Array): array containing raw line items
+     *                                 from the log files
+     */
 
-    const executePoll = (resolve, reject) => {
-      const result = fn();
-      attempts++;
+    let dataForElasticsearch = new Array()
 
-      if (maxAttempts && attempts === maxAttempts) {
-        return new Error('Exceeded max attempts');
-      } else {
-        setInterval(fn, interval);
+    for (var i in this.reader.data) {
+      const line = this.reader.data[i]
+      const structuredLine = await this._buildElasticsearchPayloadFrom(line, i)
+      dataForElasticsearch.push(
+        structuredLine
+      );
+    }
+
+    this.structuredData = dataForElasticsearch;
+    return new Uploader(this)
+  }
+
+  async _buildElasticsearchPayloadFrom(rawData, i) {
+    /**
+     * for a given log line, extract the needed data and format an Object
+     *
+     * rawData -> raw data from file or API endpoint
+     * i -> (integer): the ID for the elasticsearch document
+     *
+     * returns -> (Array): cleaned line data
+     */
+    let keysToFetch = this.reader.keysToFetch !== null ? this.reader.keysToFetch : Object.keys(rawData['txn']);
+    let structuredData = new Object()
+    let jsonifiedData;
+
+    if (typeof rawData !== 'object') {
+      try {
+        jsonifiedData = JSON.parse(rawData);
+      } catch (e) {
+        return
+      } // if cannot be parsed by JSON, return (skip the line)
+    } else {
+      jsonifiedData = rawData;
+    }
+
+    keysToFetch.forEach((key) => {
+      structuredData[key.toLowerCase()] = jsonifiedData['txn'][key]
+      structuredData['sig'] = jsonifiedData['sig']
+    })
+
+    structuredData["date"] = new Date()
+    structuredData["id"] = i
+    return structuredData
+  }
+
+
+}
+
+
+class Uploader {
+
+  constructor(parser) {
+    this.parser = parser;
+  }
+
+  async _indiceExistsFor(indexName) {
+    /**
+     * check if `indexName` exists as an elasticsearch index.
+     * indexName -> (String): the name of an index
+     */
+
+    const res = await this.client.indices.exists({
+      index: indexName
+    })
+    const exists = res.body
+
+    return exists
+  }
+
+  async _createIndex(indexName, indexMappings) {
+    /**
+     * create an elasticsearch index based on `indexName` and `indexMappings`
+     *
+     * indexName -> (String): the name of the index to create
+     * indexMappings -> (Object): the mappings of the data types for the index
+     */
+    await this.client.indices.create({
+      index: indexName,
+      body: {
+        mappings: {
+          properties: indexMappings
+        }
       }
-    };
+    }, {
+      ignore: [400]
+    })
+  }
 
-    return new Promise(executePoll)
+  async _uploadData(indexName) {
+    /**
+     * upload data to the elasticsearch index.
+     *
+     * indexName -> (String): the name of the index
+     */
+
+    const body = this.parser.structuredData.flatMap((doc) => [{
+      index: {
+        _index: indexName
+      }
+    }, doc])
+    const {
+      body: bulkResponse
+    } = await this.client.bulk({
+      refresh: true,
+      body
+    })
+    return bulkResponse
+  }
+
+  async uploadTo(client, {indexName, mappings}) {
+    /**
+     * upload data to elasticsearch
+     *
+     * indexName -> (String): the name of the index
+     * indexMappings -> (Object): the data type mappings for the fields in the index
+     */
+     this.client = client;
+
+     console.log(indexName);
+
+    let indexExists = await this._indiceExistsFor(indexName);
+
+    if (!indexExists) {
+      // if indice does not exist...
+      console.log("index does not exist, creating it now");
+      this._createIndex(indexName, indexMappings);
+    }
+
+    // index exists, we can now upload data to it.
+    await this._uploadData(indexName)
+    let {
+      body: {
+        count: count
+      }
+    } = await this.client.count({
+      index: indexName
+    })
+
+    console.log(`There are ${count} total documents in index ${indexName}`);
+  }
+
+  async testIndex(indexName, queryParams) {
+    /**
+     * helper function to test various queries on the elasticsearch index
+     *
+     * indexName -> (String): the name of the index
+     * queryParams -> (Object): the queries used to search elasticsearch
+     */
+    const {
+      body
+    } = await this.client.search({
+      index: indexName,
+      body: queryParams
+    })
+
+    console.log(body.hits.hits);
   }
 }
 
+const elasticsearchClient = new Client({
+  node: 'http://localhost:9200',
+  auth: {
+    username: 'elastic',
+    password: process.env.ES_PW
+  }
+})
 
-const hi = () => {
-  console.log("Hello!");
+let mappings = {
+  arcv: {type: "text"},
+  sig: {type: "text"},
+  fee: {type: "integer"},
+  fv: {type: "integer"},
+  gen: {type: "text"},
+  gh: {type: "text"},
+  lv: {type: "integer"},
+  note: {type: "text"},
+  snd: {type: "text"},
+  type: {type: "axfer"},
+  xaid: {type: "integer"},
+  date: {type: "date"},
+  id: {type: "text"}
 }
 
-Poll = new Poller();
-Poll.every(10).for(-1).run(hi)
+const reader = new Reader("http://127.0.0.1:8080/v2/transactions/pending", null, {
+  headers: {
+    "X-Algo-API-Token": "1e506580e964a022db4a5eb64e561240718afa6bd65e9ef1d5a2f72fe62f3775"
+  }
+})
 
-// Promise.resolve(Poll.hi().catch(console.log)).then( (res) => {
-//   console.log(res);
-// })
+
+async function run() {
+  let read = await reader.read();
+  let structure = await read.structure();
+  console.log(structure);
+  await structure.uploadTo(elasticsearchClient, {mappings: mappings, indexName: "algorand"})
+}
+
+run().catch(console.log)
